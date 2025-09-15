7d734378d1e766d9c9e30d3d9c1bc21b963fd79b
Alexandre Pinheiro Martins Bard <alexandrepinheirobard@gmail.com>
2025-09-15 17:48:50 -0300

    a

---
+++ exercicios/aula016/funcao01.js
@@ -0,0 +1,14 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return "Par!"
+    } else {
+        return "Ímpar!"
+    }
+}   
+
+console.log(parimpar(4))
+
+/* OU 
+let res = parimpar(4)
+console.log(res)
+*/
