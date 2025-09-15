9c79bd908103a2cd67b724d16bd5efaa37cfe2fb
Alexandre Pinheiro Martins Bard <alexandrepinheirobard@gmail.com>
2025-09-15 15:05:58 -0300

    a

--- exercicios/aula014ex/ex016/script.js
+++ exercicios/aula014ex/ex016/script.js
@@ -1,1 +1,11 @@
-alert(`Olá!`)
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let contar = document.getElementById('txtp')
+}
+
+if (ini.value.length == 0 || fim.value.length == 0 || contar.value.length == 0) {
+    window.alert(`[ERRO] Faltam dados!`)
+} else {
+    alert(`Tudo OK`)
+}
